89d71ba23c8aee4ca92fea209296d0635e413475
Bibek Saini <itsbibeksaini@gmail.com>
2023-01-22 23:14:00 +0530

    init projects
    - ASP.NET web api app
    - React.js client app with typescript
    - xUnit test app

---
+++ doctrine-api/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "../doctrine-clientapp";
+
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseReactDevelopmentServer(npmScript: "start");
+    }
+});
+
+app.Run();
+
---
+++ doctrine-xunit/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace doctrine_xunit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
