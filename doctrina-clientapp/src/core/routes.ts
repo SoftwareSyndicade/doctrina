69a52d56b75489be4094f10c372c9263f733dd88
Bibek Saini <itsbibeksaini@gmail.com>
2023-01-26 22:17:25 +0530

    react routes and generate react cli

---
+++ doctrina-clientapp/src/core/routes.ts
@@ -0,0 +1,12 @@
+import IRoute from "./IRoute";
+
+const ROUTES: IRoute[] = [
+    {
+        path: '/',
+        name: "",
+        component: "Home",
+        exact: true
+    }
+]
+
+export default ROUTES
