53ecc4a85185eae87e19433a788b124da01ff918
Bibek Saini <itsbibeksaini@gmail.com>
2023-01-29 15:03:28 +0530

    class component to FC

--- doctrina-clientapp/src/components/LoginPage/LoginPage.tsx
+++ doctrina-clientapp/src/components/LoginPage/LoginPage.tsx
@@ -1,114 +1,96 @@
 import { Button, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React, { Component, FC } from 'react';
+import React, { Component, FC, useState } from 'react';
 import IFormField from '../../core/IFormField';
 import styles from './LoginPage.module.scss';
 import { useNavigate } from 'react-router-dom'
-interface ILoginPageState{
-  FORM: {
-    USERNAME: "",
-    PASSWORD: ""
-  }
-  ERRORS: string[]
+
+interface LoginForm{
+  USERNAME: string,
+  PASSWORD: string
 }
 
+const LoginPage: React.FC = () => {
 
-class LoginPage extends Component<{}, ILoginPageState>{
+  const navigate = useNavigate()
 
-  constructor(props: any) {
-    super(props)
+  const [loginForm, setLoginForm] = useState<LoginForm>({
+    USERNAME: "",
+    PASSWORD: ""
+  })
 
-    this.state =({
-      FORM: {
-        USERNAME: "",
-        PASSWORD: "",
-      },      
-      ERRORS: []
+  const [errors, setErrors] = useState([])
+
+  return (
+    <Box flexDirection="row" flexWrap="wrap" display="flex" justifyContent='center' className={'wrapper'}>
+      <Box m={1} className={`${styles.loginCard}`} height='fit-content'>
+        <header className={'padding1015'}>
+          <Typography variant="h4">Welcome</Typography>
+          <Typography variant="subtitle2">Login to continue to services</Typography>
+        </header>
+
+        <ul>
+          {errors.map(err => {
+            return(<li>{err}</li>)
+          })}
+        </ul>
+
+        <div className={'padding1015'}>
+          <TextField name='USERNAME' id='USERNAME' value={loginForm.USERNAME} label="Username" fullWidth onChange={(e) => {handleChanges(e)}} style={{marginBottom:'0.75rem'}}>
+          </TextField>
+
+          <TextField name='PASSWORD' id='PASSWORD' value={loginForm.PASSWORD} label="Password" type="password" fullWidth onChange={(e) => {handleChanges(e)}} style={{marginBottom:'0.75rem'}}></TextField>
+
+          <div style={{textAlign:'right'}}>
+            <Button variant="contained" color="primary" onClick={() => {signIn()}}> Log in </Button>            
+          </div>
+        </div>
+
+        <hr/>
+
+        <div className={'padding1015'}>
+          <Typography variant="h4">Not yet registered?</Typography>
+          <Typography variant="subtitle2">Click on register to continue.</Typography>
+
+          <div style={{textAlign:'right', marginTop:'0.75rem'}}>
+            <Button variant="contained" color="primary" href='/register'> Register </Button>            
+          </div>
+        </div>
+
+      </Box>
+    </Box>
+  )
+
+  function handleChanges(e: React.ChangeEvent){
+
+    let field = e.target as HTMLInputElement
+
+    setLoginForm({
+      ... loginForm,
+      [field.name]: field.value
     })
   }
 
-  render(): React.ReactNode {
-      return(
-        <Box flexDirection="row" flexWrap="wrap" display="flex" justifyContent='center' className={'wrapper'}>
-          <Box m={1} className={`${styles.loginCard}`} height='fit-content'>
-            <header className={'padding1015'}>
-              <Typography variant="h4">Welcome</Typography>
-              <Typography variant="subtitle2">Login to continue to services</Typography>
-            </header>
-
-            <ul>
-              {this.state.ERRORS.map(err => {
-                return(<li>{err}</li>)
-              })}
-            </ul>
-
-            <div className={'padding1015'}>
-              
-              <TextField name='USERNAME' id='USERNAME' value={this.state.FORM.USERNAME} label="Username" fullWidth onChange={(e) => {this.handleChanges(e)}} style={{marginBottom:'0.75rem'}}>
-              </TextField>
-
-              <TextField name='PASSWORD' id='PASSWORD' value={this.state.FORM.PASSWORD} label="Password" type="password" fullWidth onChange={(e) => {this.handleChanges(e)}} style={{marginBottom:'0.75rem'}}>
-              </TextField>
-
-              <div style={{textAlign:'right'}}>
-                <Button variant="contained" color="primary" onClick={() => this.signIn()}> Log in </Button>            
-              </div>
-            </div>
-
-            <hr/>
-
-            <div className={'padding1015'}>
-              <Typography variant="h4">Not yet registered?</Typography>
-              <Typography variant="subtitle2">Click on register to continue.</Typography>
-
-              <div style={{textAlign:'right', marginTop:'0.75rem'}}>
-                <Button variant="contained" color="primary" href='/register'> Register </Button>            
-              </div>
-            </div>
-          </Box>
-        </Box>
-      )
-  }
-
-  async signIn(){
-
+  async function signIn(){
     fetch("/v1/auth/sign-in", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       }, 
-      body: JSON.stringify(this.state.FORM)
+      body: JSON.stringify(loginForm)
     }).then(res => {
       switch(res.status){
-        case 200:          
+        case 200:        
+          navigate("home")  
           break
         case 400:
           break
         case 401:
           res.json().then((data) => {
-            this.setState({
-              ... this.state,
-              ERRORS: data.ERRORS
-            })
+            setErrors(data.ERRORS)
           })
           break        
       }
-    })
-
-      
-    
-  }
-
-  handleChanges(e: React.ChangeEvent){
-    let field = e.target as HTMLInputElement
-
-    this.setState({
-      ... this.state,
-      FORM: {
-        ... this.state.FORM,
-        [field.name]: field.value
-      }
-      
     })
   }
 }
