43e119bbe70c95a521a12e1c9d788cafe76932d2
Bibek Saini <itsbibeksaini@gmail.com>
2023-01-29 11:52:24 +0530

    Assistance proposal

---
+++ doctrina-api/DataModels/AssistanceProposals.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace doctrine_api.DataModels
+{
+    public class AssistanceProposals
+    {
+        [Key]
+        public string ID { get; set; }
+
+        [Required]
+        public string REQUEST_ID { get; set; }
+
+        [Required]
+        public string TUTOR { get; set; }
+
+        [Required]
+        public string ACTIVE { get; set; }
+
+        [Required]
+        public int HOURS { get; set; }
+
+        [Required]
+        public double COST { get; set; }
+
+        [Required]
+        public double PERHOUR_RATE { get; set; }
+
+        public double DISCOUNT { get; set; }
+
+        [Required]
+        public string AVAILABILITY { get; set; }
+
+        [Required]
+        public DateTime CREATED_ON { get; set; }
+
+        [Required]
+        public string CREATED_BY { get; set; }
+
+        [ForeignKey("REQUEST_ID")]
+        public AssistanceReuest ASSISTANCE_REQUEST { get; set; }
+    }
+}
+
