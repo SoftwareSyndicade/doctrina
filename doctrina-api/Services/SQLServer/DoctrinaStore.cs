c3e3dcfad0d0746639f65a327f06307989e17fd2
Bibek Saini <itsbibeksaini@gmail.com>
2023-01-29 23:40:05 +0530

    db set for data models

--- doctrina-api/Services/SQLServer/DoctrinaStore.cs
+++ doctrina-api/Services/SQLServer/DoctrinaStore.cs
@@ -11,7 +11,15 @@ namespace doctrine_api.Services.SQLServer
         protected override void OnModelCreating(ModelBuilder modelBuilder) { }
 
         public DbSet<Account> ACCOUNT { get; set; }
+        public DbSet<Student> STUDENT { get; set; }
+        public DbSet<Tutor> TUTOR { get; set; }
+
+        public DbSet<Qualification> QUALIFICATIONS { get; set; }
+        public DbSet<Degree> DEGREE { get; set; }
+
         public DbSet<AssistanceReuest> ASSISTANCE_REQUEST { get; set; }
+        public DbSet<Attachments> ATTACHMENTS { get; set; }
+        public DbSet<AssistanceProposals> ASSISTANCE_PROPOSALS { get; set; }
     }
 }
 
