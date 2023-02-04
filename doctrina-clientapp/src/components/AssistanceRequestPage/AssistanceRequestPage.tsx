f138c91e988e3716865d142e6effbfc8b7c1ab34
Bibek Saini <itsbibeksaini@gmail.com>
2023-02-05 00:00:39 +0530

    converting assistance request component to functional component
    Co-Authored-By: iam-mandeepsharma <59190430+iam-mandeepsharma@users.noreply.github.com>

--- doctrina-clientapp/src/components/AssistanceRequestPage/AssistanceRequestPage.tsx
+++ doctrina-clientapp/src/components/AssistanceRequestPage/AssistanceRequestPage.tsx
@@ -1,57 +1,72 @@
-import { Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
+import { Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styles from './AssistanceRequestPage.module.scss';
 
-interface AssistanceRequestPageProps {}
+interface AssistanceRequestForm{
+  DETAILS?: string,
+  REQUEST_CATEGORY: string,
+  EDUCATION_LEVEL: string
+}
 
-class AssistanceRequestPage extends React.Component{
+const AssistanceRequestPage: React.FC = () => {
 
-  constructor(props: any){
-    super(props)
-  }
+  const [assistanceRequestForm, setAssistanceRequestForm] = useState<AssistanceRequestForm>({
+    DETAILS: "",
+    REQUEST_CATEGORY: "",
+    EDUCATION_LEVEL: ""
+  })
 
-  render(): React.ReactNode {
-      return(
-        <Box flexDirection="row" flexWrap="wrap" display="flex" justifyContent='center' className={'wrapper'}>
-          <Box m={1} className={`${styles.assistanceRequestPage}`} height='fit-content'>
-            <header className={'padding1015'} style={{textAlign:'center'}}>
-              <Typography variant="h4">Assistance request</Typography>
-              <Typography variant="subtitle2">Fill out assistance request details.</Typography>
-            </header>
+  return (
+    <Box flexDirection="row" flexWrap="wrap" display="flex" justifyContent='center' className={'wrapper'}>
+      <Box m={1} className={`${styles.assistanceRequestPage}`} height='fit-content'>
+        <header className={'padding1015'} style={{textAlign:'center'}}>
+          <Typography variant="h4">Assistance request</Typography>
+          <Typography variant="subtitle2">Fill out assistance request details.</Typography>
+        </header>
 
-            <div className={'padding1015'}>
-              <TextField multiline label="Details" fullWidth rows={4} style={{marginBottom: '0.75rem'}}></TextField>
-              <FormControl fullWidth style={{marginBottom: '0.75rem'}}>
-                <InputLabel id='REQUEST_CATEGORIES_LABLE'>Request category</InputLabel>
-                <Select label="Request category">
-                  <MenuItem value="0">None</MenuItem>
-                  <MenuItem value="1">Assistance help</MenuItem>
-                  <MenuItem value="2">Teaching</MenuItem>
-                  <MenuItem value="3">Study teaching</MenuItem>
-                  <MenuItem value="4">Advice</MenuItem>
-                </Select>                
-              </FormControl>
+        <div className={'padding1015'}>
+          <TextField multiline label="Details" name='DETAILS' value={assistanceRequestForm.DETAILS} fullWidth rows={4} style={{marginBottom: '0.75rem'}} onChange={(e) => handleChanges(e)}></TextField>
 
-              <FormControl fullWidth>
-                <InputLabel id='EDUCATION_LEVEL_LABLE'>Education level</InputLabel>
-                <Select label="Education level">
-                  <MenuItem value="0">None</MenuItem>
-                  <MenuItem value="1">High school</MenuItem>
-                  <MenuItem value="2">Diploma</MenuItem>
-                  <MenuItem value="3">Post-grad certificate</MenuItem>
-                  <MenuItem value="4">Masters</MenuItem>
-                  <MenuItem value="5">Doctrate</MenuItem>
-                </Select>                
-              </FormControl>
-            </div>
+          <FormControl fullWidth style={{marginBottom: '0.75rem'}}>
+            <InputLabel id='REQUEST_CATEGORIES_LABLE'>Request category</InputLabel>
+            <Select label="Request category" name='REQUEST_CATEGORY' value={assistanceRequestForm.REQUEST_CATEGORY} onChange={(e) => handleChanges(e)}>
+              <MenuItem value="0">None</MenuItem>
+              <MenuItem value="1">Assistance help</MenuItem>
+              <MenuItem value="2">Teaching</MenuItem>
+              <MenuItem value="3">Study teaching</MenuItem>
+              <MenuItem value="4">Advice</MenuItem>
+            </Select>                
+          </FormControl>
 
-            <div style={{textAlign:'right'}}>
-              <Button variant="contained" color="primary"> Request </Button>            
-            </div>
-          </Box>          
-        </Box>
-      )
+          <FormControl fullWidth>
+            <InputLabel id='EDUCATION_LEVEL_LABLE'>Education level</InputLabel>
+            <Select label="Education level" name='EDUCATION_LEVEL' value={assistanceRequestForm.EDUCATION_LEVEL} onChange={(e) => handleChanges(e)}>
+              <MenuItem value="0">None</MenuItem>
+              <MenuItem value="1">High school</MenuItem>
+              <MenuItem value="2">Diploma</MenuItem>
+              <MenuItem value="3">Post-grad certificate</MenuItem>
+              <MenuItem value="4">Masters</MenuItem>
+              <MenuItem value="5">Doctrate</MenuItem>
+            </Select>                
+          </FormControl>
+
+          <div style={{textAlign:'right'}}>
+            <Button variant="contained" color="primary"> Request </Button>            
+          </div>
+        </div>
+    </Box>
+    </Box>
+  )
+
+  function handleChanges(e: React.ChangeEvent | SelectChangeEvent){
+
+    let field = e.target as HTMLInputElement
+
+    setAssistanceRequestForm({
+      ... assistanceRequestForm,
+      [field.name]: field.value
+    })
   }
 }
 
--- doctrina-clientapp/src/core/routes/RouteGurad.tsx
+++ doctrina-clientapp/src/core/routes/RouteGurad.tsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet, Route } from 'react-router-dom';
 import LoginPage from '../../components/LoginPage/LoginPage';
 
 const RouteGuard = () =>{
-    const isAuth = false
+    const isAuth = true
 
     return (
         isAuth ? <Outlet/> : <Navigate to="/" />
