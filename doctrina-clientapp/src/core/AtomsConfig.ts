c7af03216a0760570d3b64289d8ab5ce6910d297
Bibek Saini <itsbibeksaini@gmail.com>
2023-02-12 15:21:08 +0530

    chore: check for jwt token expiry

--- doctrina-clientapp/src/core/AtomsConfig.ts
+++ doctrina-clientapp/src/core/AtomsConfig.ts
@@ -1,21 +1,28 @@
 import { atom } from 'jotai'
+import jwt_decode, { JwtPayload } from "jwt-decode";
 import { atomWithStorage } from 'jotai/utils'
 import Account from './auth/models/Account'
 
 const accountInit = ():Account => {
     const storedUser = localStorage.getItem("ACCOUNT")
 
-    if(storedUser != null){
-        return JSON.parse(storedUser)
-    }
-
-    return {
+    let userAccount: Account = {
         ACCESS_TOKEN: '',
         ACCOUN_TYPE: '',
         ID: '',
         NAME: '',
         IS_AUTHENTICATED: false
     }
+
+    if(storedUser != null){
+        const ua:Account = JSON.parse(storedUser)
+        let jwt = jwt_decode<JwtPayload>(ua.ACCESS_TOKEN)
+
+        if(jwt.exp !== undefined && (jwt.exp * 1000) > new Date().getSeconds())
+            userAccount = JSON.parse(storedUser)
+    }
+
+    return userAccount
 } 
 
 export const userAtom = atomWithStorage<Account>("ACCOUNT", accountInit())
