06325a24355722354c11df8c3e13afd75ed7b857
Bibek Saini <itsbibeksaini@gmail.com>
2023-03-02 22:40:15 +0530

    feat: updated gb migrations

---
+++ doctrina-api/Migrations/20230226114701_doctrinastore.v1.1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace doctrineapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class doctrinastorev11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MEETING_LINK",
+                table: "ASSISTANCE_REQUEST",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SETUP_MEETING",
+                table: "ASSISTANCE_REQUEST",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MEETING_LINK",
+                table: "ASSISTANCE_REQUEST");
+
+            migrationBuilder.DropColumn(
+                name: "SETUP_MEETING",
+                table: "ASSISTANCE_REQUEST");
+        }
+    }
+}
