81cddaeb2ec968f7739b0c5d26ad061425b221aa
Bibek Saini <itsbibeksaini@gmail.com>
2023-03-02 22:41:40 +0530

    feat: generate callender event using google calendar api

--- doctrina-api/Controllers/v1/AssistanceRequestController.cs
+++ doctrina-api/Controllers/v1/AssistanceRequestController.cs
@@ -11,6 +11,11 @@ using doctrine_api.Management.Assistance.Request;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using doctrine_api.Management.Account;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Calendar.v3.Data;
+using static Google.Apis.Calendar.v3.AclResource;
 
 namespace doctrine_api.Controllers.v1
 {
@@ -33,7 +38,7 @@ namespace doctrine_api.Controllers.v1
 
 
         [HttpPost(CRUDActions.REGISTER)]
-        public IActionResult Register([FromBody] AssistanceRequest request)
+        public async Task<IActionResult> Register([FromBody] AssistanceRequest request)
         {
             var userIdentity = HttpContext.User.Identity as ClaimsIdentity;
             var profileID = userIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
@@ -45,6 +50,67 @@ namespace doctrine_api.Controllers.v1
             assistanceRequest.CREATED_BY = profileID;
 
             var status = _assistanceRequestManager.Register(assistanceRequest);
+
+            UserCredential credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = "838726120450-ns7pqcq4fh3eaap7l52dp2kke65cfj0k.apps.googleusercontent.com",
+                    ClientSecret = "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"
+                },
+                new[] { CalendarService.Scope.Calendar },
+                "user",
+                CancellationToken.None
+            ); ;
+
+            CalendarService calendarService = new(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Doctrina tutoring"
+            });
+
+            Event calendarEvent = new()
+            {
+                Summary = "Test summary",
+                Location = "Test location",
+                Description = "Test description",
+                Start = new()
+                {
+                    DateTime = DateTime.UtcNow.AddDays(3)
+                },
+                End = new()
+                {
+                    DateTime = DateTime.UtcNow.AddDays(4)
+                },
+                Attendees = new EventAttendee[]
+                {
+                    new() { Email = "itsbibeksaini@gmail.com" },
+                    new() { Email = "manu_prashar@hotmail.com" }
+                },
+
+                ConferenceData = new()
+                {
+                    CreateRequest = new()
+                    {
+                        RequestId = Guid.NewGuid().ToString(),
+                        ConferenceSolutionKey = new() { Type = "hangoutsMeet" }
+                    },
+                }
+
+            };
+
+            try
+            {
+                EventsResource.InsertRequest insertRequest = calendarService.Events.Insert(calendarEvent, "primary");
+                insertRequest.ConferenceDataVersion = 1;
+                Event newEvent = insertRequest.Execute();
+                Console.WriteLine("Event created {0}", newEvent.HtmlLink);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
 
             if (status.IS_SAVED)
                 return StatusCode(StatusCodes.Status201Created);
